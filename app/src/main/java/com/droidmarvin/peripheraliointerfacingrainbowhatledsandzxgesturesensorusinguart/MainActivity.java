31c0518bff12856d54a8a50bd91cb9330507d6b0
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 15:28:01 +0300

    add method to handle gesture sensor events

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/MainActivity.java
@@ -6,12 +6,17 @@ import android.util.Log;
 
 import com.google.android.things.pio.PeripheralManagerService;
 import com.google.android.things.pio.UartDevice;
+import com.google.android.things.pio.UartDeviceCallback;
 
 import java.io.IOException;
 
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
     private static final String UART_GESTURE_SENSOR = "UART0";
+    private static final byte MSG_CODE_GESTURE_EVENT = (byte) 0xFC;
+    private static final byte GESTURE_CODE_SWIPE_RIGHT_EVENT = 0x01;
+    private static final byte GESTURE_CODE_SWIPE_LEFT_EVENT = 0x02;
     private UartDevice mZxGSensor;
 
     private final RGB rgb = new RGB();
@@ -43,6 +48,43 @@ public class MainActivity extends Activity {
             throw new IllegalStateException(UART_GESTURE_SENSOR + " error connecting to the pin/pin configuration", e);
         }
     }
+
+
+    private final UartDeviceCallback onUartBusHasData = new UartDeviceCallback() {
+        @Override
+        public boolean onUartDeviceDataAvailable(UartDevice uart) {
+            try {
+                byte[] buffer = new byte[3];
+                while ((uart.read(buffer, buffer.length)) > 0) {
+                    handleGestureSensorEvent(buffer);
+                }
+            } catch (IOException e) {
+                Log.e("TUT", "Cannot read device data.", e);
+            }
+
+            return true;
+        }
+
+
+        private void handleGestureSensorEvent(byte[] payload) {
+            byte messageCode = payload[0];
+            if (messageCode != MSG_CODE_GESTURE_EVENT) {
+                return;
+            }
+            byte gestureCode = payload[1];
+            if (gestureCode == GESTURE_CODE_SWIPE_RIGHT_EVENT) {
+                rgb.nextColor();
+            } else if (gestureCode == GESTURE_CODE_SWIPE_LEFT_EVENT) {
+                rgb.previousColor();
+            }
+        }
+
+        @Override
+        public void onUartDeviceError(UartDevice uart, int error) {
+            Log.e("TUT", "ERROR " + error);
+        }
+    };
+
 
     private void destroyZxGestureSensor() {
         if (mZxGSensor != null) {
