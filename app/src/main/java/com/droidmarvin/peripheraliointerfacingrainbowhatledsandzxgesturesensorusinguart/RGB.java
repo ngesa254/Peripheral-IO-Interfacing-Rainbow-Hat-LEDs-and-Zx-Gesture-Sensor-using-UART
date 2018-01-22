40445ba225024a978d1486e767757b5066caf35e
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 14:43:49 +0300

    create to connect to the rainbow hat leds

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/RGB.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/RGB.java
@@ -1,8 +1,29 @@
 package com.droidmarvin.peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart;
 
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.SpiDevice;
+
+import java.io.IOException;
+
 /**
  * Created by Ngesa on 1/22/2018.
  */
 
 public class RGB {
+    private static final String APA102_RGB_7_LED_SLAVE = "SPI0.0";
+
+    private SpiDevice mRGB;
+
+    /**
+     * Call in on create to connect to the rainbow hat leds
+     */
+    public void setUpRGB() {
+
+        PeripheralManagerService service = new PeripheralManagerService();
+        try {
+            mRGB = service.openSpiDevice(APA102_RGB_7_LED_SLAVE);
+        } catch (IOException e) {
+            throw new IllegalStateException(APA102_RGB_7_LED_SLAVE + "error opening connection", e);
+        }
+    }
 }
