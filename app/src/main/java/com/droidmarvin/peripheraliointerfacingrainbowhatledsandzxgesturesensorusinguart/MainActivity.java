357e14b0d266dc081b303df301fe1fd841e064b3
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 13:32:54 +0300

    add the barebones

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/MainActivity.java
@@ -2,11 +2,25 @@ package com.droidmarvin.peripheraliointerfacingrainbowhatledsandzxgesturesensoru
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void setupZxGestureSensor() {
+    }
+
+    private void destroyZxGestureSensor() {
     }
 }
