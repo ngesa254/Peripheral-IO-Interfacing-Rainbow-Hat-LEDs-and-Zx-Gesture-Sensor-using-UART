effb87019cfe102b4548a665fae6393db45d682b
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 13:45:50 +0300

    close ZxGesture Sensor and release resource

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        destroyZxGestureSensor();
     }
 
     private void setupZxGestureSensor() {
