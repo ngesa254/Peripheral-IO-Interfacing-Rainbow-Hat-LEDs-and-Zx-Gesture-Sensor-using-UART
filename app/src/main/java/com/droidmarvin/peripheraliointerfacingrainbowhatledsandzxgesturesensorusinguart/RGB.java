d7b1ff74735d6297214ab8799c83aa7d34430ff2
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 15:21:21 +0300

    create UartDeviceCallbac to read data from gesture sensor

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/RGB.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/RGB.java
@@ -109,7 +109,7 @@ public class RGB {
         }
     }
 
-    private void destroyRGB() {
+    public void destroyRGB() {
         if (mRGB != null) {
             try {
                 mRGB.close();
