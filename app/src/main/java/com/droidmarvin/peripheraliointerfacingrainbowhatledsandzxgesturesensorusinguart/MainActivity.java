496ebdf2ea956e8f717d0eecc5bbaecff3254d1d
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 13:39:22 +0300

    setup ZxGestureSensor connection - Live!!

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingrainbowhatledsandzxgesturesensorusinguart/MainActivity.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.UartDevice;
+
 import java.io.IOException;
 
 public class MainActivity extends Activity {
+    private static final String UART_GESTURE_SENSOR = "UART0";
+    private UartDevice mZxGSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +24,15 @@ public class MainActivity extends Activity {
     }
 
     private void setupZxGestureSensor() {
+        try {
+            PeripheralManagerService service = new PeripheralManagerService();
+            mZxGSensor = service.openUartDevice(UART_GESTURE_SENSOR);
+        } catch (IOException e) {
+            throw new IllegalStateException(UART_GESTURE_SENSOR + " error connecting to the pin", e);
+        }
     }
 
     private void destroyZxGestureSensor() {
     }
 }
+
